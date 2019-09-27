8ca81a97103b153e583ddb1a5511037931ec19f5
Akilia <pm@akilia.net>
2019-09-27 12:19:50 +0200

    Squelette Membre par défaut

--- equipes_fonctions.php
+++ equipes_fonctions.php
@@ -12,3 +12,14 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
+
+
+/**
+ * Virer les espaces blancs dans un numéro de téléphone
+ *
+ * @param string $text
+ * @return string
+ */
+function remove_whitespace($text) {
+	return str_replace(' ', '', $text);
+}
