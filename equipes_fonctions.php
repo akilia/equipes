6d01fe9689ab16b980bbc8351de5a8ebdc6280c2
Akilia <pm@akilia.net>
2020-02-14 10:50:54 +0100

    Pour un membre, pouvoir choisir dans quel équipe il doit figurer

--- equipes_fonctions.php
+++ equipes_fonctions.php
@@ -23,3 +23,40 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function remove_whitespace($text) {
 	return str_replace(' ', '', $text);
 }
+
+
+/**
+ * Retourner le tableau id_equipe/nom des equipes existantes
+ * Utile pour les #SAISIE -> datas
+ *
+ * @example
+ *  [(#SAISIE{checkbox, id_equipe, Choisir une case à cocher, datas=[(#VAL{4}|equipes_liste_equipes)]})]
+ *  [(#SAISIE{radio, categorie, datas=[(#VAL{1}|equipes_liste_equipes{valeur_par_defaut})]})]
+ *  [(#SAISIE{selection, categorie, datas=[(#VAL{1}|equipes_liste_equipes{valeur_par_defaut})]})]
+ * 
+ * @param int id_rubrique
+ *		ID de la rubrique ou l'on veut recupérer la liste des équipes
+ * @param string defaut
+ *		Valeur par defaut du tableau d'equipe
+ *
+ * @return array
+ *		array(id_equipe -> nom, etc.)
+ */
+function equipes_liste_equipes($id_rubrique, $defaut = null) {
+	$liste_equipes = array();
+
+	if ($defaut) {
+		$liste_equipes = array('' => $defaut);
+	}
+
+	$res = sql_allfetsel('id_equipe, nom', 'spip_equipes', 'id_rubrique='.intval($id_rubrique));
+	foreach ($res as $value) {
+		$add_equipes[$value['id_equipe']] = $value['nom'];
+	}
+
+	if (isset($add_equipes)){
+		$liste_equipes += $add_equipes;
+	}
+
+	return $liste_equipes;
+}
